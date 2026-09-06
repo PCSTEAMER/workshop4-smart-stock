b1a596e1f009180bc6f19714b022e4a6675cffb4
Phubet <lomabut33@gmail.com>
2026-09-07 00:04:42 +0700

    Switch SMTP to port 465 with SSL to fix timeout on Render

--- stock_project/stock_project/settings.py
+++ stock_project/stock_project/settings.py
@@ -105,11 +105,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='lomabut33@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='igjsaahtaawaakbm')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_PORT = 465              
+EMAIL_USE_SSL = True         
+EMAIL_USE_TLS = False         
+EMAIL_TIMEOUT = 10            
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
 
 # ==========================================
 # Default Primary Key
