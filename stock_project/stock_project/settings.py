941ec35f88aaf0832caa743d4ed44ee30d607b9d
Phubet <lomabut33@gmail.com>
2026-09-07 02:05:35 +0700

    Update SMTP settings to Brevo

--- stock_project/stock_project/settings.py
+++ stock_project/stock_project/settings.py
@@ -104,14 +104,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Email Configuration (.env)
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465              
-EMAIL_USE_SSL = True         
-EMAIL_USE_TLS = False         
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587              
+EMAIL_USE_SSL = False     
+EMAIL_USE_TLS = True       
 EMAIL_TIMEOUT = 10            
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 # ==========================================
 # Default Primary Key
