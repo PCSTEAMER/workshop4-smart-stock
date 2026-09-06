3f02fc2c07d893cc2e94c9e9cb1329671c3f2c71
Phubet <lomabut33@gmail.com>
2026-09-06 20:08:18 +0700

    Enable media serving and upload part images

--- stock_project/stock_project/urls.py
+++ stock_project/stock_project/urls.py
@@ -1,29 +1,20 @@
 """
 URL configuration for stock_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings  
-from django.conf.urls.static import static  
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('parts_app.urls')),  # โยนทางไปให้ parts_app จัดการ
-    path('accounts/', include('accounts.urls')), # 👈 เพิ่มบรรทัดนี้เข้ามา
+    path('', include('parts_app.urls')),
+    path('accounts/', include('accounts.urls')),
+    
+    # ดึงไฟล์ภาพจาก media มาแสดงผลบนโฮสต์จริง
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-# บรรทัดนี้สำคัญมาก! ทำให้ตอนเรารันเทส ระบบสามารถแสดงรูปภาพที่เราอัปโหลดได้
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
