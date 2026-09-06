06a5b03d14bebc664a58862e78ea1f026d7a9923
Phubet <lomabut33@gmail.com>
2026-09-07 02:55:06 +0700

    Fix clean email settings syntax

--- stock_project/stock_project/settings.py
+++ stock_project/stock_project/settings.py
@@ -105,10 +105,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587              
-EMAIL_USE_SSL = False     
-EMAIL_USE_TLS = True       
-EMAIL_TIMEOUT = 10            
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
@@ -117,4 +117,3 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 # Default Primary Key
 # ==========================================
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#ทดสอบว่าไฟล์อัพเดทจริงไหมบน GitHub
